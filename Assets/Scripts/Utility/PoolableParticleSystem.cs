bc5334686492a571a6d207cb45bf40e1f7fe6b33
Henri Niva <hegemege@gmail.com>
2018-11-23 21:00:24 +0200

    Add utility games

---
+++ Assets/Scripts/Utility/PoolableParticleSystem.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PoolableParticleSystem : MonoBehaviour
+{
+    private ParticleSystem _ps;
+
+    void Awake()
+    {
+        _ps = GetComponentInChildren<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        if (!_ps.IsAlive())
+        {
+            _ps.Clear();
+            gameObject.SetActive(false);
+            return;
+        }
+
+        if (!_ps.isPlaying)
+        {
+            _ps.Play();
+        }
+    }
+}
