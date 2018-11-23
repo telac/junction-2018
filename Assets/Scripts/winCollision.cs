c62ee237e6c5c5ce89e73449b87aa7e9c8701d85
Roskis <antti.karkinen@aalto.fi>
2018-11-24 00:44:02 +0200

    simple win area

---
+++ Assets/Scripts/winCollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class winCollision : MonoBehaviour
+{
+    SpriteRenderer _spriteRenderer;
+
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.color = Color.red;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            _spriteRenderer.color = Color.green;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            _spriteRenderer.color = Color.red;
+        }
+    }
+}
