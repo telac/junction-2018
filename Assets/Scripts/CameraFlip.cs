ee0b888b60e6379a71413ecb0617005399594b44
Henri Niva <hegemege@gmail.com>
2018-11-24 00:53:15 +0200

    Dual camera

---
+++ Assets/Scripts/CameraFlip.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class CameraFlip : MonoBehaviour
+{
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, new Vector2(1, -1), new Vector2(0f, 1f));
+    }
+}
