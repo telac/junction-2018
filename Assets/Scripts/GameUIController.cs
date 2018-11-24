37967e59185ccbeae6652446e7e688e12530f8bf
Nicholas Saarela <nicholas.saarela@gmail.com>
2018-11-24 19:37:31 +0200

    Added UI stuff

---
+++ Assets/Scripts/GameUIController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUIController : MonoBehaviour
+{
+    public void OnPauseButtonPress()
+    {
+        Debug.Log("TÄTTÄÄÄ");
+    }
+}
