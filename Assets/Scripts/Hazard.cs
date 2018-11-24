f40d5aa194bbe94da068e9eceb1158780a99f4a2
Roskis <antti.karkinen@aalto.fi>
2018-11-24 19:48:15 +0200

    basic hazard

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "LightBall")
+        {
+            GameManager.Instance.gameOver();
+        }
+    }
+
+
+}
