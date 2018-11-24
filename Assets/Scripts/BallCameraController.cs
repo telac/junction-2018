06a355bddff466ea9c3c67aca3c1a92981d2188d
Henri Niva <hegemege@gmail.com>
2018-11-24 16:08:55 +0200

    Add shader stuff

--- Assets/Scripts/BallCameraController.cs
+++ Assets/Scripts/BallCameraController.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteAlways]
 public class BallCameraController : MonoBehaviour
 {
     public BallType BallType;
+
+    public Material SceneCameraMaterial;
 
     // TODO: Tracking
 
     void Awake()
     {
+        if (!GameManager.Instance) return;
+
         if (BallType == BallType.Dark)
         {
             GameManager.Instance.DarkCamera = this;
