8b6cf8b5749a964a7005da7ee9c4d31d4a6fa63b
Henri Niva <hegemege@gmail.com>
2018-11-24 17:32:40 +0200

    make camera follow the balls horizontally

--- Assets/Scripts/BallCameraController.cs
+++ Assets/Scripts/BallCameraController.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteAlways]
 public class BallCameraController : MonoBehaviour
 {
     public BallType BallType;
 
-    // TODO: Tracking
+    public GameObject FollowTarget;
+    public float FollowSmoothing;
+    public float LeftDeadZone;
+    public float RightDeadZone;
+
+    private Camera _camera;
+    private Vector3 _initialPosition;
 
     void Awake()
     {
         if (!GameManager.Instance) return;
+
+        _initialPosition = transform.position;
+        _camera = GetComponent<Camera>();
 
         if (BallType == BallType.Dark)
         {
@@ -21,5 +29,30 @@ public class BallCameraController : MonoBehaviour
         {
             GameManager.Instance.LightCamera = this;
         }
+    }
+
+    void Update()
+    {
+        if (FollowTarget == null) return;
+
+        var aspectRatio = _camera.aspect;
+        var leftEdge = transform.position.x - _camera.orthographicSize * aspectRatio * LeftDeadZone;
+        var rightEdge = transform.position.x + _camera.orthographicSize * aspectRatio * RightDeadZone;
+
+        var targetX = FollowTarget.transform.position.x;
+
+        var newX = transform.position.x;
+
+        if (targetX < leftEdge)
+        {
+            newX -= Mathf.Abs(targetX - leftEdge);
+        }
+
+        if (targetX > rightEdge)
+        {
+            newX += Mathf.Abs(targetX - rightEdge);
+        }
+
+        transform.position = new Vector3(newX, _initialPosition.y, _initialPosition.z);
     }
 }
