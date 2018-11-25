2c96213e930b3c91fc71914545d100c847d2df18
Roskis <antti.karkinen@aalto.fi>
2018-11-25 09:26:51 +0200

    pendulum level

---
+++ Assets/Scripts/Pendulum.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pendulum : MonoBehaviour
+{
+    public float offset;
+    private Vector3 _startPosition;
+    void Awake() {
+        _startPosition = gameObject.transform.position;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = _startPosition + new Vector3(0.0f, 4.0f * Mathf.Sin(Time.time + offset), 0.0f);
+    }
+}
